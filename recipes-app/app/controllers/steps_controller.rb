74430667726dc09232380c280018621f09d2b0b7
Brendan Brown <pbbrown1112@gmail.com>
2019-06-17 14:47:30 -0500

    intial steps_controller setup

--- recipes-app/app/controllers/steps_controller.rb
+++ recipes-app/app/controllers/steps_controller.rb
@@ -1,3 +1,33 @@
 class StepsController < ApplicationController
+    
+    skip_before_action :verify_authenticity_token  
 
+    def edit
+        @step = Step.find(params[:id])
+    end
+
+    def create
+        # byebug
+        Step.create(step_params)
+
+    end
+
+    def update
+        # byebug
+        Step.update(step_params)
+    end
+
+    def destroy
+        
+        @Step = Step.find(params[:id])
+        @step.destroy
+    
+    end
+
+    def step_params
+        params.permit(
+            :description        
+        )
+
+    end
 end
