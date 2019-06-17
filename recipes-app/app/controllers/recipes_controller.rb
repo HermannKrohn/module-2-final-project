7b61c84c849c7a950b502e26592f3938c85a99ac
Brendan Brown <pbbrown1112@gmail.com>
2019-06-17 15:25:58 -0500

    intial recipes controller setup

--- recipes-app/app/controllers/recipes_controller.rb
+++ recipes-app/app/controllers/recipes_controller.rb
@@ -1,3 +1,36 @@
 class RecipesController < ApplicationController
 
+    skip_before_action :verify_authenticity_token  
+
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def create
+        # byebug
+        Recipe.create(recipe_params)
+
+    end
+
+    def update
+        # byebug
+        @recipe = Recipe.find(params[:id])
+        @recipe.update(recipe_params)
+    end
+
+    def destroy
+        
+        @recipe = Recipe.find(params[:id])
+        @recipe.destroy
+    
+    end
+
+    def recipe_params
+        params.permit(
+            :title,
+            :category        
+        )
+
+    end
+
 end
