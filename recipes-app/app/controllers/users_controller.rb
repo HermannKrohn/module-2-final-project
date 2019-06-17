00c11176c8d2439f3c4ae4f702cf8e0473a5c457
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-06-17 14:54:17 -0500

    initial index page and sign up functionality without validation and hashing passwords

--- recipes-app/app/controllers/users_controller.rb
+++ recipes-app/app/controllers/users_controller.rb
@@ -1,3 +1,26 @@
 class UsersController < ApplicationController
 
+    def create_account #DONT FORGET TO VALIDATE INPUTS
+        # byebug
+        new_user = User.new(user_params[:user])
+        new_user.save
+        redirect_to "/index/#{new_user.id}"
+    end
+
+    def index
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(
+            user: [
+                :username,
+                :first_name,
+                :last_name,
+                :age,
+                :password_hash
+            ]
+        )
+    end
+
 end
--- recipes-app/config/routes.rb
+++ recipes-app/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :recipes
+  resources :ingredients
+  resources :steps
+
+  get '/index/:id', to: "users#index"
+  get '/sign_up', to: 'users#sign_up'
+  post '/sign-up', to: 'users#create_account'
 end
 
-resources :users
-resources :recipes
-resources :ingredients
-resources :steps
 
-get '/sign_up', to: 'users#sign_up'
-post '/sign-up', to: 'users#create_account'
 
 
