9fb1b6b31b06a9e641fcfb7b428f8bf37f5dfb3e
Brendan Brown <pbbrown1112@gmail.com>
2019-06-17 14:21:37 -0500

    intial ingredients controller

--- recipes-app/app/controllers/ingredients_controller.rb
+++ recipes-app/app/controllers/ingredients_controller.rb
@@ -1,3 +1,33 @@
 class IngredientsController < ApplicationController
 
+    skip_before_action :verify_authenticity_token  
+
+
+    def edit
+        @ingredient = Ingredient.find(params[:id])
+    end
+
+    def update
+        # byebug
+        Ingredient.create(ingredient_params)
+
+    end
+
+    def destroy
+        
+        @ingredient = Ingredient.find(params[:id])
+        @ingredient.destroy
+    
+    end
+
+    def ingredient_params
+        params.permit(
+            :name,
+            :category,
+            :quantity,
+            :units
+        )
+
+    end
+
 end
