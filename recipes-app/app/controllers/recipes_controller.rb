46446f1357a2d983160d66b00cc8e10631a380fe
Brendan Brown <pbbrown1112@gmail.com>
2019-06-18 15:10:10 -0500

    added some flash errors to recipes controller

--- recipes-app/app/controllers/recipes_controller.rb
+++ recipes-app/app/controllers/recipes_controller.rb
@@ -2,20 +2,50 @@ class RecipesController < ApplicationController
 
     skip_before_action :verify_authenticity_token  
 
+    def new
+        @recipes = Recipe.all
+        if (flash[:form_errors])
+            @errors = flash[:form_errors]
+        else
+            @recipes = Recipe.all
+        end        
+
+    end
+    
     def edit
+        if(flash[:form_errors])
+            @errors = flash[:form_errors]
+        else
         @recipe = Recipe.find(params[:id])
+        end
+
     end
 
     def create
         # byebug
-        Recipe.create(recipe_params)
+
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.valid?
+            @recipe.save
+        else
+            flash[:form_errors] = @recipe.errors.messages
+            redirect_to "/index/#{session[:id]}"
+        end
 
     end
 
     def update
         # byebug
+
         @recipe = Recipe.find(params[:id])
-        @recipe.update(recipe_params)
+        @recipe.assign_attributes(recipe_params)
+        if @recipe.valid?
+            @recipe.save
+            redirect_to 
+        else
+            flash[:form_errors] = @recipe.errors.messages
+            redirect_to "/recipes/#{@recipe.id}/edit"
+
     end
 
     def destroy
