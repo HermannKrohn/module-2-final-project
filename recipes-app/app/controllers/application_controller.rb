8e056be303f21dafc845c202fb2c74672c1156f7
Hermann Krohn <krohnhermannacosta@gmail.com>
2019-06-21 14:06:51 -0500

    something

--- recipes-app/app/controllers/application_controller.rb
+++ recipes-app/app/controllers/application_controller.rb
@@ -2,22 +2,14 @@ class ApplicationController < ActionController::Base
     before_action :check_cookie_expiry
 
     def check_cookie_expiry
-        # puts session[:user_id]
         if session[:user_id] != nil
-            # puts session[:expires_at]
-            # puts Time.current
             if session[:expires_at] < Time.current
                 session[:user_id] = nil
-                # Make flash to let user know session expired
-                # puts 'REDIRECTTIINNNNNNNNGGGGGG SESSION EXPIRED'
                 redirect_to "/login"
             else
-                # puts 'INCREASING SESSION TIME'
                 session[:expires_at] = Time.current + 5.minutes
             end
         else
-            # Flash message to let user know they have not logged in
-            # puts 'YOUR NOT LOGGED IN! PLEASE LOG IN'
             redirect_to "/login"
         end
     end
